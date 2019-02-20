5ff49f1fb378143e04d10fc8908a74b2578f686b
hendisantika <hendisantika@yahoo.co.id>
2019-02-21 05:59:18 +0700

    #6 - Add views.py file

--- core/views.py
+++ core/views.py
@@ -1,1 +1,36 @@
 # Create your views here.
+from django.views.generic import TemplateView, CreateView, DetailView, FormView
+
+from core.forms import BootstrapEventForm, XDSoftEventForm, FengyuanChenEventForm, DateForm
+from core.models import Event
+
+
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+
+
+class EventDetailView(DetailView):
+    model = Event
+
+
+class ManualFormView(FormView):
+    form_class = DateForm
+    template_name = 'core/manual.html'
+
+
+class XDSoftDateTimePickerView(CreateView):
+    model = Event
+    form_class = XDSoftEventForm
+    template_name = 'core/xdsoft_event_form.html'
+
+
+class BootstrapDateTimePickerView(CreateView):
+    model = Event
+    form_class = BootstrapEventForm
+    template_name = 'core/bootstrap_event_form.html'
+
+
+class FengyuanChenDatePickerView(CreateView):
+    model = Event
+    form_class = FengyuanChenEventForm
+    template_name = 'core/fengyuanchen_event_form.html'
